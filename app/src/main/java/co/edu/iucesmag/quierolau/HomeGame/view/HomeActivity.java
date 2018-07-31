8795bb03b167f71381eea450cdd9ae93c44c0db7
JonMid <jamideros@hotmail.com>
2018-07-31 18:35:17 -0500

    Cerrar sesion

--- app/src/main/java/co/edu/iucesmag/quierolau/HomeGame/view/HomeActivity.java
+++ app/src/main/java/co/edu/iucesmag/quierolau/HomeGame/view/HomeActivity.java
@@ -2,6 +2,7 @@ package co.edu.iucesmag.quierolau.HomeGame.view;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
@@ -17,8 +18,10 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import co.edu.iucesmag.quierolau.Login.view.LoginActivity;
 import co.edu.iucesmag.quierolau.Questions.view.QuestionsActivity;
 import co.edu.iucesmag.quierolau.R;
+import co.edu.iucesmag.quierolau.SplashScreen.view.SplashScreenActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -49,6 +52,11 @@ public class HomeActivity extends AppCompatActivity {
         onStartGame();
     }
 
+    public void onClickButtonSingOut(View view) {
+        Dialog dialog = showAlertDialog();
+        dialog.show();
+    }
+
     public void onStartGame(){
         dialogProgressBar = showProgressBarDialog();
         dialogProgressBar.show();
@@ -68,11 +76,39 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onSingOut(){
+        sharedPreferences = getSharedPreferences("PreferencesQuieroLaU", Context.MODE_PRIVATE);
+        sharedPreferences.edit().clear().commit();
+
+        Intent intent = new Intent(this, SplashScreenActivity.class);
+        startActivity(intent);
+    }
+
     public Dialog showProgressBarDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
         builder.setView(inflater.inflate(R.layout.dialog_progress_bar, null));
         builder.setCancelable(false);
+        return builder.create();
+    }
+
+    public Dialog showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Información");
+        builder.setMessage("¿Estás segur@ de cerrar sesión? Recuerda que puedes jugar cuantas veces desees y así ser uno de los ganadores de un fabuloso premio.");
+        builder.setCancelable(false);
+        builder.setPositiveButton(R.string.str_alert_button_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                onSingOut();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //
+            }
+        });
         return builder.create();
     }
 }
